6bd1adcc99eb2939981293dbc0e1a5e0c63bfdf6
Oluwaseyi Aimudo <oluwaseyiaimudo@Thes-MacBook-Air.local>
2022-07-06 12:31:30 +0100

    added more UI

--- src/app/pages/trending/trending.component.ts
+++ src/app/pages/trending/trending.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-trending',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trending.component.scss']
 })
 export class TrendingComponent implements OnInit {
+  isBookmarked: Boolean = false
+  bookmark = '../../../assets/images/icon-bookmark-empty.svg'
+  bookmarkId:number = 1
+  homeData:any = []
+  trending:any = []
+  homeMovies:any = []
 
-  constructor() { }
-
+  constructor(private data: DataService) { }
   ngOnInit(): void {
+    this.homeData = this.data.getData()
+    this.homeData.map((data:any) => {
+      if(data.isTrending === true){
+        this.trending.push(data)
+      }else{
+        this.homeMovies.push(data)
+      }
+    })
+    console.log(this.trending) 
+  }
+
+  toggleBookmark(id: number){
+    console.log(this.isBookmarked)
+    this.isBookmarked =! this.isBookmarked
+    
+    this.bookmarkId = id
+    // if(this.bookmarkId === id){
+    //   this.isBookmarked =! this.isBookmarked
+    //   // if(this.isBookmarked === false){
+    //   //   this.bookmark = '../../../assets/images/icon-bookmark-empty.svg'
+    //   // }else{
+    //   //   this.bookmark = '../../../assets/images/icon-bookmark-full.svg'
+    //   // }
+    // }
+    console.log(id, this.bookmarkId)
+    
   }
 
 }
